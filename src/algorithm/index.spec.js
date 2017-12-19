0a04ca17fff94c2ad8d13f25f105a39c43746011
Craig Martin <craig@kujoyardwear.com>
2017-12-19 12:27:51 -0500

    reformat tests a bit

--- src/algorithm/index.spec.js
+++ src/algorithm/index.spec.js
@@ -65,8 +65,7 @@ describe('Ties:', () => {
     });
   });
 
-  describe('advanced sequence', () => {
-    // THIS TEST AND THE NEXT TOGETHER SHOW LIMITS
+  describe('more advanced forms of ties', () => {
 
     // A fallback vote should not cost your candidate their chance to win (tie)
     test('1', () => {
@@ -83,21 +82,6 @@ describe('Ties:', () => {
       });
     });
 
-    // Geeze.
-    // Three way tie??
-    // {"percentage": 33.33, "received": 1, "success": true, "total": 3, "winner": ["rubio", "cruz", "clinton"]}
-    // or cruz/rubio win??
-    // {"percentage": 66.67, "received": 2, "success": true, "total": 3, "winner": ["cruz", "rubio"]}
-    // THOUGHT
-    // I think rubio / cruz should win
-    // Especially look at the next expect testcase
-    // OLD THOUGHT
-    // -- I think three way tie.
-    // -- "A fallback vote shouldn't cost your candidate their chance of winning"
-    // -- True, but each ballot must distill to exactly one vote.
-    // -- The fallback vote doesn't cost the candidate, still wins (ties).
-    // -- This is, though, the closest this methodology comes to reneging on "never splits the vote"
-    // -- It doesn't though. Their candidate still wins (via tie) but allows another to also win.
     test('2', () => {
       expect(getWinner([
         ['rubio', 'cruz'],
@@ -111,7 +95,6 @@ describe('Ties:', () => {
         percentage: 66.67
       });
     });
-
 
     test('3', () => {
       expect(getWinner([
@@ -128,7 +111,6 @@ describe('Ties:', () => {
       });
     });
 
-    // Not Cruz
     test('4.0', () => {
       expect(getWinner([
         ['rubio', 'cruz'],
@@ -145,7 +127,6 @@ describe('Ties:', () => {
         percentage: 50.00
       });
     });
-    // Not Cruz (again)
     test('4.1', () => {
       expect(getWinner([
         ['rubio', 'cruz'],
@@ -162,7 +143,6 @@ describe('Ties:', () => {
         percentage: 50.00
       });
     });
-    // Not Cruz (again)
     test('4.2', () => {
       expect(getWinner([
         ['rubio', 'cruz'],
@@ -179,7 +159,6 @@ describe('Ties:', () => {
         percentage: 50.00
       });
     });
-    // Not Cruz (again)
     test('4.3', () => {
       expect(getWinner([
         ['rubio', 'cruz'],
@@ -213,7 +192,6 @@ describe('Ties:', () => {
   });
 
   describe('tie showing: not weighted ranks, but true vote', () => {
-    // Teachable Example
     // Not weighted ranking. True votes.
     test('1', () => {
       expect(getWinner([
@@ -232,7 +210,6 @@ describe('Ties:', () => {
   });
 
   describe('two way tie handled correctly (not take into account fallback)', () => {
-    // Important one. Mistakenly had rubio winning previously.
     // Fallback vote should not cost your candidate their chance to win (tie)
     test('1', () => {
       expect(getWinner([
@@ -288,7 +265,6 @@ describe('Ties:', () => {
   });
 
   describe('another', () => {
-    // Teachable Example
     // Not weighted ranking (wherein clinton would win). True votes.
     test('1', () => {
       expect(getWinner([
@@ -305,10 +281,95 @@ describe('Ties:', () => {
       });
     });
   });
+
+  describe('advanced', () => {
+    // A fallback vote should not cost your candidate their chance to win (tie)
+    // Closest this algorithm comes to "splitting" the vote - carson
+    // would have won (with no tiers, and a larger majority) if trump had
+    // not been a candidate. But those folks canidate still "won" (via tie).
+    // Because trump had a chance of winning (tieing), his fallback votes must
+    // not be factored in.
+    // "It's not splitting the vote if you 'win'"
+    test('1', () => {
+      expect(getWinner([
+        ['rubio'],
+        ['rubio'],
+        ['rubio'],
+        ['trump', 'carson'],
+        ['trump', 'carson'],
+        ['trump', 'carson'],
+        ['carson'],
+        ['carson']
+      ])).toEqual({
+        success: true,
+        names: ['rubio', 'trump'],
+        received: 3,
+        total: 8,
+        percentage: 37.50
+      });
+    });
+
+    // Below example is INCORRECT.
+    /*
+      rubio    bush    bush    trump   trump   trump   carson    carson
+      carson   rubio   rubio   carson  carson  carson  rubio     trump
+      -----------------------------------------------------------------
+
+      rubio    bush    bush    TRUMP   TRUMP   TRUMP   carson    carson
+      carson   rubio   rubio   carson  carson  carson  rubio     trump
+
+      RUBIO    bush    bush    trump   trump   trump   carson    carson
+      carson   RUBIO   RUBIO   carson  carson  carson  RUBIO     trump
+
+      rubio    bush    bush    trump   trump   trump   CARSON    CARSON
+      carson   rubio   rubio   CARSON  CARSON  CARSON  rubio     trump
+
+      (Interestingly, Carson doesn't get the vote from the first person,
+      since Rubio is the leader at that point)
+    */
+
+    // Below is CORRECT:
+    /*
+      rubio    bush    bush    trump   trump   trump   carson    carson
+      carson   rubio   rubio   carson  carson  carson  rubio     trump
+      -----------------------------------------------------------------
+
+      rubio    bush    bush    TRUMP   TRUMP   TRUMP   carson    carson
+      carson   rubio   rubio   carson  carson  carson  rubio     trump
+
+      RUBIO    bush    bush    trump   trump   trump   carson    carson
+      carson   RUBIO   RUBIO   carson  carson  carson  RUBIO     trump
+
+      RUBIO    bush    bush    TRUMP   TRUMP   TRUMP   carson    carson
+      carson   RUBIO   RUBIO   carson  carson  carson  RUBIO     TRUMP
+    */
+    test('2', () => {
+      expect(getWinner([
+        ['rubio', 'carson'],
+        ['bush', 'rubio'],
+        ['bush', 'rubio'],
+        ['trump', 'carson'],
+        ['trump', 'carson'],
+        ['trump', 'carson'],
+        ['carson', 'rubio'],
+        ['carson', 'trump']
+      ])).toEqual({
+        success: true,
+        names: ['rubio', 'trump'],
+        received: 4,
+        total: 8,
+        percentage: 50.00
+        // names: ['carson'],
+        // received: 5,
+        // total: 8,
+        // percentage: 62.50
+      });
+    });
+  });
 });
 
 describe('Winning:', () => {
-  describe('winning with minority (backward compatible with standard voting metholodogy', () => {
+  describe('winning with minority (backward compatible with standard voting metholodogy)', () => {
     test('1', () => {
       expect(getWinner([
         ['rubio'],
@@ -326,7 +387,7 @@ describe('Winning:', () => {
     });
   });
 
-  describe('winning with majority (backward compatible with standard voting metholodogy', () => {
+  describe('winning with majority (backward compatible with standard voting metholodogy)', () => {
     test('1', () => {
       expect(getWinner([
         ['rubio'],
@@ -346,6 +407,9 @@ describe('Winning:', () => {
 
   describe('successful fallback', () => {
     test('1', () => {
+      // No sense of "weighted" (bush has more "weighted" 1st place points)
+      // than rubio. Rather, each ballot is a true vote. Bush can't win,
+      // but rubio can, so the votes go to rubio.
       expect(getWinner([
           ['rubio'],
           ['rubio'],
@@ -385,8 +449,8 @@ describe('Winning:', () => {
       });
     });
 
-    // Notice: Rubio does not win because fallback
-    // choices are only evaulated from the losing voter's choices.
+    // Notice: Rubio does not win because fallbacks
+    // are only evaulated from the losing voters' ballots.
     test('1', () => {
       expect(getWinner([
         ['kasich', 'rubio'],
@@ -448,11 +512,8 @@ describe('Winning:', () => {
       });
     });
 
-    // Notice: Rubio does not win because fallback
-    // choices are only evaulated from the losing voter's choices.
-    // More challenging version of the previous test:
-    // Kasich didn't have majority, so it didn't give him the win.
-    // For some reason, wants to do {"percentage": 55.56, "received": 5, "success": true, "total": 9, "winner": ["rubio", "bush"]}
+    // Notice: Rubio does not win because fallbacks
+    // are only evaulated from losing voters' ballots.
     test('3', () => {
       expect(getWinner([
         ['rubio'],
@@ -473,8 +534,8 @@ describe('Winning:', () => {
       });
     });
 
-    // Notice: In the second round, carson was not chosen because fallback
-    // choices are only evaluated from the losing voters' choices.
+    // Notice: In the second round, carson was not chosen because fallbacks
+    // are only evaluated from losing voters' ballots.
     test('4', () => {
       expect(getWinner([
         ['cruz'],
@@ -498,11 +559,11 @@ describe('Winning:', () => {
     });
 
 
-    // Not a tie
-    // This is similiar to the below "Geeze" example's final stage.
-    // Unrelated, doesn't end with 100%. Should it? No - losers votes
-    // should remain their highest choice? Yes - reconcile the votes
-    // to the winner. Show both percentages, I suppose.
+    // Trump has majority of votes outright, so there was no need to recurse
+    // through. Doing so (as is shown in the next test) would result in him
+    // getting all 6 votes.
+    // Todo: do we want to examine ballots in `main` (the wrapper function
+    // for which is exposed as `getWinner`) to account for this?
     test('5', () => {
       expect(getWinner([
         ['trump', 'carson'],
@@ -519,6 +580,7 @@ describe('Winning:', () => {
         percentage: 66.67
       });
     });
+    // cf test above
     // Ensures get "trueLeader"
     test('6', () => {
       expect(getWinner([
@@ -543,16 +605,13 @@ describe('Winning:', () => {
     });
 
 
-    // Geeze.
     // Trump is winning in first round with 4.
     // Carson does not beat him because fallbacks only counted from loser.
     // However, Rubio does beat him in the second round (with 5).
     // This could mistakenly "free up" the trump/carson votes to be either
     // order in the quest to try to beat rubio.
-    // Trump should win this. Not a Trump/Carson tie.
     //
-    // Need logic to figure out who has more higher votes relative to tie
-    // candiates (not all)
+    // Ensures logic of `getTrueWinners`
     test('7', () => {
       expect(getWinner([
         ['rubio'],
@@ -577,95 +636,9 @@ describe('Winning:', () => {
   });
 
 
-  describe('advanced: flips twice', () => {
-    // Not a flip twice, just trying to help understand why "1" below is failing
-    // Geesh, I suppose "A fallback vote should not cost your candidate their
-    // chance to win (tie)". But then it seems like "trump" split "carson"s
-    // vote. Or no, because it was successful..
-    test('0', () => {
-      expect(getWinner([
-        ['rubio'],
-        ['rubio'],
-        ['rubio'],
-        ['trump', 'carson'],
-        ['trump', 'carson'],
-        ['trump', 'carson'],
-        ['carson'],
-        ['carson']
-      ])).toEqual({
-        success: true,
-        names: ['rubio', 'trump'],
-        received: 3,
-        total: 8,
-        percentage: 37.50
-      });
-    });
+  describe('advanced', () => {
 
-    // Here, there are not more "carson"s than "trumps", so "carson"
-    // is never chosen because "trump" can "win", and so the honor
-    // is given to him
-
-    // Thus: below is INCORRECT. Trump can tie rubio and so does.
-    // "It's not splitting the vote if you 'win'"
-    /*
-      rubio    bush    bush    trump   trump   trump   carson    carson
-      carson   rubio   rubio   carson  carson  carson  rubio     trump
-      -----------------------------------------------------------------
-
-      rubio    bush    bush    TRUMP   TRUMP   TRUMP   carson    carson
-      carson   rubio   rubio   carson  carson  carson  rubio     trump
-
-      RUBIO    bush    bush    trump   trump   trump   carson    carson
-      carson   RUBIO   RUBIO   carson  carson  carson  RUBIO     trump
-
-      rubio    bush    bush    trump   trump   trump   CARSON    CARSON
-      carson   rubio   rubio   CARSON  CARSON  CARSON  rubio     trump
-
-      (Interestingly, Carson doesn't get the vote from the first person,
-      since Rubio is the leader at that point)
-      Ending ballots would look like:
-      [['rubio', 'carson'], ['rubio'], ['rubio'], ['carson'], ['carson'], ['carson'], ['carson', 'rubio'], ['carson', 'trump']]
-    */
-
-    // Below is CORRECT:
-    /*
-      rubio    bush    bush    trump   trump   trump   carson    carson
-      carson   rubio   rubio   carson  carson  carson  rubio     trump
-      -----------------------------------------------------------------
-
-      rubio    bush    bush    TRUMP   TRUMP   TRUMP   carson    carson
-      carson   rubio   rubio   carson  carson  carson  rubio     trump
-
-      RUBIO    bush    bush    trump   trump   trump   carson    carson
-      carson   RUBIO   RUBIO   carson  carson  carson  RUBIO     trump
-
-      RUBIO    bush    bush    TRUMP   TRUMP   TRUMP   carson    carson
-      carson   RUBIO   RUBIO   carson  carson  carson  RUBIO     TRUMP
-    */
     test('1', () => {
-      expect(getWinner([
-        ['rubio', 'carson'],
-        ['bush', 'rubio'],
-        ['bush', 'rubio'],
-        ['trump', 'carson'],
-        ['trump', 'carson'],
-        ['trump', 'carson'],
-        ['carson', 'rubio'],
-        ['carson', 'trump']
-      ])).toEqual({
-        success: true,
-        names: ['rubio', 'trump'],
-        received: 4,
-        total: 8,
-        percentage: 50.00
-        // names: ['carson'],
-        // received: 5,
-        // total: 8,
-        // percentage: 62.50
-      });
-    });
-
-    test('2', () => {
       expect(getWinner([
         ['rubio', 'carson'],
         ['bush', 'rubio'],
@@ -685,7 +658,7 @@ describe('Winning:', () => {
       });
     });
 
-    test('3', () => {
+    test('2', () => {
       expect(getWinner([
         ['rubio', 'carson'],
         ['rubio'],
